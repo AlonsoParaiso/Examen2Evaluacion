261b0937d42911915ceee99ca8928543cd3ee4df
AlonsoParaiso <alonpahe2@gmail.com>
2024-02-26 13:39:48 +0100

    4apartado

--- Assets/Scripts/Firework.cs
+++ Assets/Scripts/Firework.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,19 +13,22 @@ public class Firework : MonoBehaviour
     private Rigidbody2D _rb;
     private SpriteRenderer _rend;
     private int _count = 0;
-    private Vector2 _dir = Vector2.up;
+    private Vector2 dir = Vector2.up;
     private float currentTime, timeToExplode;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rend = GetComponent<SpriteRenderer>();
+        timeToExplode=UnityEngine.Random.Range(minFireworks, maxFireworks);
+        _rend.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        _rb.velocity = dir * force;
     }
 }
--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
+    public AudioClip timeClip;
+    public KeyCode timeKey;
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(timeKey))
+        {
+            StartCoroutine(Witch());
+        }
+    }
+    IEnumerator Witch()
+    {
+        yield return 0;
     }
 }
